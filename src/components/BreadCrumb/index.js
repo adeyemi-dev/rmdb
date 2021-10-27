0c931d9e51ae24debda18cadbb32a4cfa7d4f1d4
adeyemi-dev <lagudaadeyemi@yahoo.com>
2021-10-27 20:09:11 +0100

    movie api

---
+++ src/components/BreadCrumb/index.js
@@ -0,0 +1,22 @@
+import react from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+// Styles
+import { Wrapper, Content } from "./BreadCrumb.styles";
+
+const BreadCrumb = ({ movieTitle }) => (
+  <Wrapper>
+    <Content>
+      <Link to='/'>
+        <span>Home</span>
+      </Link>
+      <span>|</span>
+      <span>{movieTitle}</span>
+    </Content>
+  </Wrapper>
+);
+
+BreadCrumb.propsTypes = {
+  movieTitle: PropTypes.string,
+};
+export default BreadCrumb;
